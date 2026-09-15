1ea1d3edd805eeea11d4de3a6355e88c76edc288
pumpkinbluepie <averymudgett@gmail.com>
2026-09-15 19:39:27 -0400

    Initialize customtkinter app with settings

--- ui/home.py
+++ ui/home.py
@@ -1,1 +1,12 @@
+import customtkinter
+from ui.settings import theme
 
+customtkinter.set_appearance_mode(theme)
+
+app = customtkinter.CTk()
+app.geometry("640x480")
+app.title("astro weather")
+app.resizable(False, False)
+
+
+app.mainloop()
