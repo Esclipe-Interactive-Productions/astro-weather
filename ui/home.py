a12540a4775497100a395dbe0f453c90a623bd0a
pumpkinbluepie <averymudgett@gmail.com>
2026-09-15 20:37:17 -0400

    Refactor customtkinter imports and usage

--- ui/home.py
+++ ui/home.py
@@ -1,12 +1,21 @@
-import customtkinter
+import customtkinter as ctk
 from ui.settings import theme
+from weatherapi import data
 
-customtkinter.set_appearance_mode(theme)
+button = ctk.CTkButton
+label = ctk.CTkLabel
+entry = ctk.CTkEntry
 
-app = customtkinter.CTk()
+ctk.set_appearance_mode(theme)
+
+app = ctk.CTk()
 app.geometry("640x480")
 app.title("astro weather")
 app.resizable(False, False)
 
+maintitle = label(app, text="astro weather", font=("Times", 15))
+maintitle.place(x=10, y=5)
+searchentry = entry(app, width=160, font=("Times", 11))
+searchentry.place(x=100, y=5)
 
 app.mainloop()
