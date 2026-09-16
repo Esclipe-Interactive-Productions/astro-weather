a43fc5ccad4348347480afeb6ed9086068b07a9a
pumpkinbluepie <averymudgett@gmail.com>
2026-09-15 20:28:04 -0400

    Add weather API integration with OpenWeatherMap

--- weatherapi.py
+++ weatherapi.py
@@ -1,1 +1,18 @@
+import os
+import requests
+from dotenv import load_dotenv
 
+load_dotenv()
+
+weatherapikey = os.getenv("weatherapikey")
+
+response = requests.get(
+    "https://api.openweathermap.org/data/2.5/weather",
+    params={
+        "q": "New York",
+        "appid": weatherapikey,
+        "units": "imperial"
+    }
+)
+
+data = response.json()
