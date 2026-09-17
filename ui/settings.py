77f77427c6f761683a5571293c6e27c3edad7269
pumpkinbluepie <averymudgett@gmail.com>
2026-09-17 15:48:56 -0400

    Add additional settings to load from JSON

--- ui/settings.py
+++ ui/settings.py
@@ -3,3 +3,10 @@ import json
 with open("data/settings.json", "r") as file:
     settings = json.load(file)
     theme = settings["theme"]
+    temperatureunit = settings["temperature unit"]
+    speedunit = settings["speed unit"]
+    pressureunit = settings["pressure unit"]
+    distanceunit = settings["distance unit"]
+    savesearches = settings["save searches"]
+    nofications = settings["notifcations"]
+    weatheralerts = settings["weather alerts"]
