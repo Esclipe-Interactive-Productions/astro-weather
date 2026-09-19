0a861b97f8be1c61c8ac7a9d122f6cc1dc8ee91e
pumpkinbluepie <averymudgett@gmail.com>
2026-09-19 10:16:44 -0400

    Enhance weather search functionality in home.py
    Added weatherdata variable and weatherdescription function to handle weather data display.

--- ui/home.py
+++ ui/home.py
@@ -2,17 +2,19 @@ import customtkinter as ctk
 from ui.settings import theme
 from weatherapi import searchcity as getweather
 
-
 button = ctk.CTkButton
 label = ctk.CTkLabel
 entry = ctk.CTkEntry
 
 ctk.set_appearance_mode(theme)
 
+weatherdata = None
+
 def searchcity():
     cityname = searchentry.get()
     print(cityname)
     getweather(cityname)
+    weatherdata = getweather(cityname)
 
 app = ctk.CTk()
 app.geometry("640x480")
@@ -25,5 +27,11 @@ searchentry = entry(app, width=160, font=("Times", 11), placeholder_text="insert
 searchentry.place(x=100, y=5)
 searchbutton = button(app, text="search", font=("Times", 11), width=55, fg_color="green", hover_color="dark green", command=searchcity)
 searchbutton.place(x=275, y=5)
+settingsbutton = button(app, text="settings", font=("Times", 11), fg_color="green", hover_color="dark green", width=70)
+settingsbutton.place(x=550, y=5)
 
+def weatherdescription():
+    descriptionlabel = label(app, text="", font=("Times", 14))
+    descriptionlabel.place(x=40, y=200)
+    return weatherdata
 app.mainloop()
